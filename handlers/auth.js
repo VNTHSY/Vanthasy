0f4569a2abdc45d1c27a103b0b2a115ba38aa9ad
zurravie <radityprtama@gmail.com>
2025-01-27 12:07:38 +0700

    refactor: improve handlers and add translation

--- handlers/auth.js
+++ handlers/auth.js
@@ -22,7 +22,7 @@ function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('login');
+  res.redirect('/login');
 }
 
 module.exports = { isAuthenticated }
--- handlers/init.js
+++ handlers/init.js
@@ -1,41 +1,37 @@
-const { db } = require("../handlers/db.js");
-const config = require("../config.json");
-const { v4: uuidv4 } = require("uuid");
-const CatLoggr = require("cat-loggr");
+const { db } = require('../handlers/db.js');
+const config = require('../config.json');
+const { v4: uuidv4 } = require('uuid');
+const CatLoggr = require('cat-loggr');
 const log = new CatLoggr();
 
 async function init() {
-  const Overvoid = await db.get("Overvoid_instance");
-  if (!Overvoid) {
-    log.init("this is probably your first time starting Overvoid, welcome!");
-    log.init("you can find documentation for the panel at Overvoid.dev");
+    const overvoid = await db.get('overvooid_instance');
+    if (!overvoid) {
+        log.init('this is probably your first time starting overvoid, welcome!');
+        log.init('you can find documentation for the panel at overvoid.xyz');
 
-    let imageCheck = await db.get("images");
-    if (!imageCheck) {
-      log.error(
-        "before starting Overvoid for the first time, you didn't run the seed command!"
-      );
-      log.error("please run: npm run seed");
-      log.error(
-        "if you didn't do it already, make a user for yourself: npm run createUser"
-      );
-      process.exit();
-    }
+        let imageCheck = await db.get('images');
+        if (!imageCheck) {
+            log.error('before starting overvoid for the first time, you didn\'t run the seed command!');
+            log.error('please run: npm run seed');
+            log.error('if you didn\'t do it already, make a user for yourself: npm run createUser');
+            process.exit();
+        }
 
-    let OvervoidId = uuidv4();
-    let setupTime = Date.now();
+        let overvoidId = uuidv4();
+        let setupTime = Date.now();
+        
+        let info = {
+            overvoidId: overvoidId,
+            setupTime: setupTime,
+            originalVersion: config.version
+        }
 
-    let info = {
-      OvervoidId: OvervoidId,
-      setupTime: setupTime,
-      originalVersion: config.version,
-    };
+        await db.set('overvoid_instance', info)
+        log.info('initialized overvoid panel with id: ' + overvoidId)
+    }        
 
-    await db.set("Overvoid_instance", info);
-    log.info("initialized Overvoid panel with id: " + OvervoidId);
-  }
-
-  log.info("init complete!");
+    log.info('init complete!')
 }
 
-module.exports = { init };
+module.exports = { init }
